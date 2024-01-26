be8e53dedba8cc1b57010650952534408f4412df
Patrick Ly <112744444447551+patrickly1@users.noreply.github.com>
2024-01-25 19:39:17 -0500

    Finished console version of tictactoe game

--- javascript.js
+++ javascript.js
@@ -38,9 +38,50 @@ function Cell() {
     return { addValue, getValue };
 }
 
+function Game(
+    player1 = "Player One", player2 = "Player Two"
+    ) {
+    const board = gameBoard();
 
+    const players = [
+        {
+            name: player1,
+            symbol: "X"
+        },
+        {
+            name: player2, 
+            symbol: "O"
+        }
+    ];
+
+    let activePlayer = players[0];
+
+    const switchPlayerTurn = () => {
+        activePlayer = activePlayer === players[0] ? player[1] : players[0];
+    }
+
+    const getActivePlayer = () => activePlayer;
+
+    const printNewRound = () => {
+        board.printBoard();
+        return `${getActivePlayer().name}'s turn`
+    };
+
+    const playRound = (row, column) => {
+        console.log(
+            `${getActivePlayer().name} places ${getActivePlayer().symbol}}`
+        );
+        board.makeMove(row, column, getActivePlayer().symbol);
+
+        switchPlayerTurn();
+        printNewRound();
+    };
+
+    printNewRound();
+}
 
 
 const myGameBoard = gameBoard();
 myGameBoard.makeMove(0, 0, "X");
+myGameBoard.makeMove(1, 1, "O");
 console.log(myGameBoard.printBoard());
